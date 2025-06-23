eddefca0abb3a3b9996a8efa60530f3523b38bda
Linyx79 <linyx0709@foxmail.com>
2025-06-23 17:06:14 +0800

    范围判断

--- Tetris/CTetrimino.h
+++ Tetris/CTetrimino.h
@@ -1,29 +1,34 @@
 #ifndef CTETRIMINO_H
 #define CTETRIMINO_H
 
-enum All_Shape {O_shape, I_shape, Z_shape, S_shape, L_shape, J_shape, T_shape};
+#include <QPoint>
+
+enum All_Shape {O_shape, I_shape, Z_shape, S_shape, L_shape, J_shape, T_shape, None_shape};
 enum Direction {Up, Down, Left, Right};
 
 class CTetrimino{
-    static constexpr int Shape[7][4][4] =
+    static constexpr int Shape[8][4][2] =
     {
-        {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}},  //O_shape
-        {{0, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}},  //I_shape
-        {{0, 0, 0, 0}, {0, 1, 1, 0}, {0, 0, 1, 1}, {0, 0, 0, 0}},  //Z_shape
-        {{0, 0, 0, 0}, {0, 1, 1, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}},  //S_shape
-        {{0, 0, 0, 0}, {0, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 1, 0}},  //L_shape
-        {{0, 0, 0, 0}, {0, 0, 1, 0}, {0, 0, 1, 0}, {0, 1, 1, 0}},  //J_shape
-        {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 1, 0, 0}, {1, 1, 1, 0}}   //T_shape
+        {{1, 1}, {1, 2}, {2, 1}, {2, 2}},  // O_shape
+        {{0, 2}, {1, 2}, {2, 2}, {3, 2}},  // I_shape
+        {{1, 2}, {2, 2}, {2, 1}, {3, 1}},  // Z_shape
+        {{1, 1}, {2, 1}, {2, 2}, {3, 2}},  // S_shape
+        {{1, 0}, {1, 1}, {1, 2}, {2, 1}},  // L_shape
+        {{1, 0}, {2, 0}, {2, 1}, {2, 2}},  // J_shape
+        {{1, 1}, {2, 1}, {3, 1}, {2, 2}},  // T_shape
+        {{0, 0}, {0, 0}, {0, 0}, {0, 0}}   // None_shape
     };
 private:
-    int Tetri_shape[4][4];
+    int Tetri_shape[4][2];
     All_Shape type;
-    int left, right, up, down, pos_x, pos_y; // 上下左右边界，及绝对坐标
+    int left, right, up, down; // 上下左右边界（相对坐标）
+    QPoint pos; // 绝对坐标（以左下角为参考点）
 
     void set_shape(All_Shape shape); // 根据形状设置图形
     void creat_next_shape(); // 随机生成下一个图形
 
-    void move(Direction dire);
+    // void move(Direction dire);
+    void border(); // 确定图形边界（相对坐标）
 
 public:
     CTetrimino();
